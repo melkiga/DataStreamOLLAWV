dc70ca6fddfb0b064cc1abf28d57dea71d79cd26
GaMe <game194991@gmail.com>
2017-09-12 13:48:45 -0400

    Added code for getting the model context description

--- OLLASolver.java
+++ OLLASolver.java
@@ -20,11 +20,6 @@ public class OLLASolver extends AbstractClassifier {
 	private static final long serialVersionUID = 7225375378266360793L;
 	
 	/**
-	 * Logger for debugging.
-	 */
-	private static Logger log;
-	
-	/**
 	 * SVM Penalty Parameter C, command line, default value = 1.0
 	 */
 	public FloatOption cOption = new FloatOption("C", 'c', "SVM Penalty Parameter.", 1.0);
@@ -56,17 +51,56 @@ public class OLLASolver extends AbstractClassifier {
 	 * Option to standardize data, default = 1
 	 */
 	public IntOption standardizeOption = new IntOption("standardize",'p',"Standardize data, 0 mean 1 stdv",0);
+	/**
+	 * Seed value, default = 1
+	 */
+	public IntOption seedOption = new IntOption("seed",'r',"Seed value.",1);
 	
 	/**
-	 * Sets options for model.
+	 * Data Header Variables
+	 */
+	public int num_data;
+	public int dim;
+	public int num_classes;
+	public int standardize;
+	/**
+	 * Logger for debugging.
+	 */
+	private static Logger log = new Logger();
+	/**
+	 * Holds SVM Hyper-parameters.
+	 */
+	private static SVMParameters params = new SVMParameters();
+	
+	/**
+	 * Sets options for model and initializes header for data.
 	 */
 	@Override
 	public void setModelContext(InstancesHeader context){
-		SVMParameters params = new SVMParameters();
+		// Set SVM parameters from command-line
 		params.setC(cOption.getValue());
 		params.setGamma(gOption.getValue());
 		params.setEpochs(eOption.getValue());
 		params.setTol(tOption.getValue());
+		// get data header
+		this.num_data = context.numInstances();
+		this.dim = context.numAttributes();
+		this.num_classes = context.numClasses();
+		this.standardize = standardizeOption.getValue();
+		// if verbose, print out the model header
+		if(vOption.getValue() == 1){
+			log.printBarrier();
+			log.print(this.getPurposeString());
+			log.printBarrier();
+			log.print(this.getModelContextString());
+			log.printBarrier();
+			log.printFormatted("Total number of data: %d\n", this.num_data);
+			log.printFormatted("Dimensionality: %d\n", this.dim);
+			log.printFormatted("Number of classes: %d\n", this.num_classes);
+			log.printFormatted("Standardize data: %d\n", this.standardize);
+			log.printFormatted("Seed: %d\n", this.randomSeed);
+			log.printBarrier();
+		}
 	}
 	
 	@Override
@@ -104,7 +138,8 @@ public class OLLASolver extends AbstractClassifier {
 	@Override
 	public void trainOnInstanceImpl(Instance inst) {
 		// TODO Trains the classifier on a given instance
-		
+		log.print("ERROR: trainOnInstanceImpl was called and no implementation for this yet.\n");
+		System.err.println("trainOnInstanceImpl::This sould not happen.");
 	}
 
 	@Override
@@ -117,8 +152,23 @@ public class OLLASolver extends AbstractClassifier {
 	 */
 	@Override
     public String getPurposeString() {
-        return "OnLine Learning Algorithm using Worst Violator (OLLAWV), VK and GM (2017).";
+        return "OnLine Learning Algorithm using Worst Violator (OLLAWV), VK and GM (2017).\n";
     }
+	
+	/**
+	 * Prints the model context.
+	 */
+	public String getModelContextString(){
+		StringBuffer buff = new StringBuffer();
+		buff.append("Data Standardization: "+standardize+"\n");
+		buff.append("SVM Parameter C: "+params.getC()+"\n");
+		buff.append("RBF Parameter gamma: "+params.getGamma()+"\n");
+		buff.append("Using Bias: "+params.getBetta()+"\n");
+		buff.append("Pruning tolerance: "+params.getTol()+"\n");
+		buff.append("Number of Epochs: "+params.getEpochs()+"\n");
+		buff.append("Using Change Detection: "+changeOption.getValue()+"\n");
+        return buff.toString();
+	}
 	
 	/**
 	 *  Prints parameter out to string builder (auto-generated inherited). 
