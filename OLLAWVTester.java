b4590ea4e65de0b800c3336a3979f05f1bf733c1
GaMe <game194991@gmail.com>
2017-09-21 14:58:05 -0400

    Changed run environment to be like command line
    > Jorge and I fixed the way the code is run to be exactly like how MOA would run it from the command line but using Alberto's code.
    > we now only need to set the options in a string that is built and call doTask()

--- OLLAWVTester.java
+++ OLLAWVTester.java
@@ -1,12 +1,12 @@
 package vcu.edu.datastreamlearning.ollawv;
 
-import com.yahoo.labs.samoa.instances.Instances;
+import moa.DoTask;
 
-import moa.streams.generators.RandomRBFGenerator;
 
 public class OLLAWVTester {
 	
 	public static void main(String[] args){
+		
 		Logger log = new Logger();
 		/* Set up data configuration. */
 		int numChunks = 2;
@@ -14,8 +14,15 @@ public class OLLAWVTester {
 		int dim = 2;
 		int seed = 1;
 		int numClasses = 2;
+		String[] localArgs = {"moa.tasks.EvaluateInterleavedChunks -S ", 
+				"-s \"generators.RandomRBFGenerator -r "+seed+" -i "+seed+" -c "+numClasses+" -a "+dim+"\"", 
+				"-l \"vcu.edu.datastreamlearning.ollawv.OLLASolver -v 1\"", 
+				"-i "+chunkSize*numChunks, 
+				"-f "+dim, 
+				"-c "+chunkSize};
 		
 		/* Print configuration. */
+		log.printBarrier();
 		log.printFormatted("Number of chunks: %d\n", numChunks);
 		log.printFormatted("Number of instances: %d\n", chunkSize);
 		log.printFormatted("Dimensionality: %d\n", dim);
@@ -23,53 +30,8 @@ public class OLLAWVTester {
 		log.printFormatted("Seed: %d\n", seed);
 		log.printBarrier();
 		
-		/* Create stream & set stream options based on configuration. */
-		RandomRBFGenerator stream = new RandomRBFGenerator();
-		stream.instanceRandomSeedOption.setValue(seed);
-		stream.modelRandomSeedOption.setValue(seed);
-		stream.numClassesOption.setValue(numClasses);
-		stream.numAttsOption.setValue(dim);
-		stream.numCentroidsOption.setValue(2);
-		stream.instanceRandomSeedOption.setValue(seed);
-		stream.numAttsOption.setValue(dim);
-		stream.prepareForUse();
-		
-		/* Initialize model */
-		OLLASolver model = new OLLASolver();
-		model.cOption.setValue(1.0);
-		model.gOption.setValue(0.5);
-		model.changeOption.setValue(0);
-		model.vOption.setValue(1);
-		model.setModelContext(stream.getHeader());
-		
-		/* Prequential Test-then-train on each chunk */
-		int numSamplesProcessed = 0;
-		for(int c = 0; c < numChunks; c++){
-			/* Build each chunk */
-			Instances chunk = new Instances(stream.getHeader(), chunkSize);
-			for(int i = numSamplesProcessed; i < (chunkSize + numSamplesProcessed); i++){
-				chunk.add(stream.nextInstance().getData());
-			}
-			numSamplesProcessed += chunkSize;
-			
-			/* Print data */
-			if(chunkSize < 25){
-				log.printBarrier();
-				log.printFormatted("CHUNK #%d\n",c);
-				log.printData(chunk, chunkSize, dim);
-				log.printBarrier();
-			}
-			
-			if(c != 0){
-				// test
-				// TODO: write up test code
-				// TODO: write up timer for testing
-			}
-			
-			// train
-			// TODO: write up timer for training
-			
-		}
+		/* Calls Evaluate Interleaved Chunks */
+		DoTask.main(localArgs);
 	}
 
 }
