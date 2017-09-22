fade03c537690e40d3e06847e84a5f07f4bb7b78
GaMe <game194991@gmail.com>
2017-09-22 13:27:27 -0400

    Added close for log in tester since it's a 'resource leak'

--- OLLAWVTester.java
+++ OLLAWVTester.java
@@ -30,6 +30,8 @@ public class OLLAWVTester {
 		log.printf("Seed: %d\n", seed);
 		log.printBarrier();
 		
+		log.close();
+		
 		/* Calls Evaluate Interleaved Chunks */
 		DoTask.main(localArgs);
 	}
